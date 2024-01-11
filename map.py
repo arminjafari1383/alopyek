e1feee3a345da12988e8b0958bd130042fc1d018
armin <arminjafri138386@gmail.com>
2024-01-11 08:22:18 +0330

    start create api for map

---
+++ map.py
@@ -0,0 +1,10 @@
+import requests
+api_key = 'web.f87d9553ba724c05afab2cad8931d20f'
+data = {
+    'type' : 'motorcycle',
+    'origin' : ',',
+    'destination' : ','
+}
+requests.get(
+    url=https://api.neshan.org/v4/direction?parameters
+)
